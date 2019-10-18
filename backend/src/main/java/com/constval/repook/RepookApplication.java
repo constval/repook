ae8826356edc2a9046bd980520273416677a49c4
darivs <dtack192@gmail.com>
2019-10-18 23:49:57 +0200

    Initialize backend

---
+++ backend/src/main/java/com/constval/repook/RepookApplication.java
@@ -0,0 +1,13 @@
+package com.constval.repook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/constval/repook/RepookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constval.repook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
