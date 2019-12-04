3a7984fb044003a50606bb66ab903163f80bd340
dkraemer <dennis.kraemer@mail.de>
2019-12-04 19:33:14 +0100

    removes exclude on springapplication

--- backend/src/main/java/com/constval/repook/RepookApplication.java
+++ backend/src/main/java/com/constval/repook/RepookApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class RepookApplication {
 
 	public static void main(String[] args) {
